d32fce6edb2f47acae9809a368c4643eb9276703
Che Mok <chemok78@gmail.com>
2016-06-23 04:48:46 +0000

    make it better

--- server.js
+++ server.js
@@ -7,10 +7,18 @@ var passport = require('passport');
 var session = require('express-session');
 
 var app = express();
+//create express app
+
 require('dotenv').load();
+//Loads environment variables from .env file
+//The process.env property returns an object containing the user environment
+//Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env.
+
 require('./app/config/passport')(passport);
+//require configuration file for passport
 
 mongoose.connect(process.env.MONGO_URI);
+//connect to mondodb database using mongoose
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
